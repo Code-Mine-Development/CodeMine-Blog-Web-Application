0ce8208c978f7b6c1f97b0f5026afe2f6d17b77b
Adam Grabek <adam@grabek.io>
2016-11-22 17:46:31 +0100

    Quote fix for code style compliance CMW-9

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PortfolioComponent} from "./portfolio.component";
+import {PortfolioComponent} from './portfolio.component';
 
 @NgModule({
   imports: [
