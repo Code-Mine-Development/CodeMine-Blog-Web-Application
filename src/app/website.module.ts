604a17c740bd5e42ae75d3309a2d6dfcfed556e3
Adam Grabek <adam@grabek.io>
2016-11-23 17:51:26 +0100

    Build fixes for bugs that emerged after angular version bump

--- src/app/website.module.ts
+++ src/app/website.module.ts
@@ -1,14 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {WebsiteComponent} from './website.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
   ],
   declarations: [WebsiteComponent],
   bootstrap: [WebsiteComponent]
